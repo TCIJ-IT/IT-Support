e6b0152c0d71aed583491a9e854fbb3bf5dea17b
cti-jeddah <anasmonti80@gmail.com>
2026-09-23 15:26:43 +0300

    Delete push-config.js

--- push-config.js
+++
@@ -1,5 +0,0 @@
-// Public values only. The setup command fills backendUrl after deployment.
-window.CTE_PUSH_CONFIG = {
-  appUrl: 'https://anasmonti80-png.github.io/CTE-Jeddah-IT-Support/',
-  backendUrl: ''
-};
